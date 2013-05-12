950058106bcbea6afd6be6fbeb070984deb73e2e
root <root@axighi-Aspire-4741.(none)>
2013-05-12 12:45:10 +0800

    delete tradespot

--- tradespot/footer.php
+++
@@ -1,6 +0,0 @@
-
-	<footer class="body_footer">
-	    <p>Copyright &copy;2013 Axighi Studio.Inc</p>
-	</footer>
-</body>
-</html>
--- tradespot/js/functions.js
+++
@@ -1,8 +0,0 @@
-function showDigitals () {
-	digitalButton=document.getElementById('digital');
-	digitalButton.addEventListener('click',generateQuery,false);
-}
-function generateQuery(){
-	document.write();
-}
-window.onload=showDigitals;
