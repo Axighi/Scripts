f1e28d7772f4979e7fc3236ea9913b07a4833db3
Dax Longving <daxlongving@hotmail.com>
2013-05-12 21:47:08 +0800

    vc reconstrction

---
+++ list.h
@@ -0,0 +1,56 @@
+#include <iostream>
+
+//define a list node struct
+typedef struct NODE
+{
+	int data;  //data
+	struct NODE *next;  //a pointer to next node
+
+	//first constructor,only set next=null
+	NODE()
+	{
+		next=NULL;
+	}
+
+	//second constructor,set a value to data
+	NODE(int a)
+	{
+		data=a;
+		next=NULL;
+	}
+}NODE;
+//NODE==struct NODE_    define a new node
+
+class List
+{
+public:
+	List();
+	~List();
+
+	//get size of the list
+	int getSize();
+	
+	//remove node i from list
+	void removeAt(int i);
+
+	//insert a at k,before node k
+	void insert(int a,int k);
+
+	//insert after the tail
+	void insert(int a);
+
+	//remove node i from list
+	void remove(int k);
+
+	//travel the list
+	void travel();
+
+
+private:
+	NODE head;  //the head node of the list
+	int size;  //length of the list
+};
+
+
+
+
