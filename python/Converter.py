9e57f42295210b8295bdee1f08299dc6f42f2b9e
Azriel Liang <daxlongving@gmail.com>
2014-12-21 21:05:38 +0800

    1221

---
+++ python/Converter.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# import Tkinter 
+
+def convert(degMinSec):
+  # print(degMinSec)
+  degreePosition=degMinSec.find("°")
+  degree = degMinSec[:degreePosition]
+  # print(degree)
+  minutePosition = degMinSec.find("\'")
+  minute = degMinSec[degreePosition + 1: minutePosition]
+  minute = minute[1:]
+  # print(minute)
+  secondPosition = degMinSec.find('''"''')
+  second = degMinSec[minutePosition + 1: secondPosition]
+  # print(second)
+  return str(float(degree) + float(minute)/60 + float(second)/3600)[:12]
+
+# def start(file):
+file=raw_input("请输入文件位置：")
+f=open(file)
+longtitudeColumn = open("longtitude.txt", 'w')
+latitudeColumn = open("latitude.txt", 'w')
+lines = f.readlines()
+for i in range(len(lines)):
+  if lines[i] == "\n":
+    longtitudeColumn.write('\n')
+    latitudeColumn.write('\n')
+  else:
+    coordinate = lines[i].split()
+    # print(coordinate)
+    longtitude = coordinate[0]
+    latitude = coordinate[1]
+    convertedLongtitude = convert(longtitude)
+    convertedLatitude = convert(latitude)
+    longtitudeColumn.write(convertedLongtitude)
+    latitudeColumn.write(convertedLatitude)
+longtitudeColumn.close()
+latitudeColumn.close()
+f.close()
+
+
+
+
+
